eeac68e8ff5256f43a2991c3772b830afe795746
Pavel Vasilev <76531243+GyrateTiara8831@users.noreply.github.com>
2020-12-31 02:50:00 +0300

    Add files via upload

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -5,97 +5,47 @@ interface Expression{
 }
 
 class NumberExpression implements Expression{
-    Integer number;
-    public NumberExpression(Integer number){
-        this.number = number;
-    }
-    @Override
-    public int interpret() {
-        return number;
-    }
-}
+    Integer real;
+    Integer imaginary;
 
-class MinusExpression implements Expression{
-    Expression left;
-    Expression right;
-    public MinusExpression(Expression left, Expression right){
-        this.left = left;
-        this.right = right;
-    }
-    @Override
-    public int interpret() {
-        return left.interpret() - right.interpret();
-    }
-}
+    public NumberExpression(){}
 
-class PlusExpression implements Expression{
-    Expression left;
-    Expression right;
-    public PlusExpression(Expression left, Expression right){
-        this.left = left;
-        this.right = right;
+    public NumberExpression(Integer real, Integer imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
     }
-    @Override
-    public int interpret() {
-        return left.interpret() + right.interpret();
-    }
-}
 
-class MultExpression implements Expression{
-    Expression left;
-    Expression right;
-    public MultExpression(Expression left, Expression right){
-        this.left = left;
-        this.right = right;
-    }
-    @Override
-    public int interpret() {
-        return left.interpret() * right.interpret();
-    }
-}
-
-class DivExpression implements Expression{
-    Expression left;
-    Expression right;
-    public DivExpression(Expression left, Expression right){
-        this.left = left;
-        this.right = right;
-    }
-    @Override
-    public int interpret() {
-        return left.interpret() / right.interpret();
-    }
-}
-
-class Context{
-    Expression evaluate(String s){
-        int pos = s.length()-1;
-        while (pos > 0) {
-            if(Character.isDigit(s.charAt(pos))){
-                pos--;
-            }
-            else{
-                Expression left = evaluate(s.substring(0, pos));
-                Expression right = new NumberExpression(Integer.valueOf(s.substring(pos+1, s.length())));
-                char operator = s.charAt(pos);
-                switch (operator){
-                    case '-': return new MinusExpression(left, right);
-                    case '+': return new PlusExpression(left, right);
-                    case '*': return new MultExpression(left, right);
-                    case '/': return new DivExpression(left, right);
-                }
-            }
+    public String toString(){
+        if(imaginary>0){
+            return real + "+" + imaginary + "i";
+        }else{
+            return real + "-" + imaginary + "i";
         }
-        int result = Integer.valueOf(s);
-        return new NumberExpression(result);
+    }
+
+    public NumberExpression Add( NumberExpression cB ) {
+        NumberExpression sum = new NumberExpression();
+
+        sum.real     = real      + cB.real;
+        sum.imaginary = imaginary + cB.imaginary;
+
+        return (sum);
+    }
+
+    @Override
+    public int interpret() {
+        return 0;
     }
 }
+
 
 public class Main {
     public static void main(String[] args) {
-        Context context = new Context();
-        Expression e = context.evaluate("2+6/4*3");
-        System.out.println(e.interpret());
-
+        NumberExpression cA = new NumberExpression(1, 2);
+        NumberExpression cB = new NumberExpression(1,5);
+        System.out.println("Complex number cA = " + cA.toString() );
+        System.out.println("Complex number cB = " + cB.toString() );
+        NumberExpression cC = cA.Add(cB);
+        System.out.println("Complex   cA + cB = " + cC.toString() );
     }
 }
